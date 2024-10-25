f6cae2ab79358bcc89e81293cf220da770888233
Bagus Arya <aryabagus453@gmail.com>
2024-10-25 16:03:48 +0800

    server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    const nilai = req.query.nilai
+    res.send(nilai);
+});
+
+app.get('/about', (req, res) => {
+    res.send('Ini adalah halaman about');
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
