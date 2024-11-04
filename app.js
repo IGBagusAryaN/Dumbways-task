27ca8a05c58cf6daa58deeac54d55df5a0c746e7
Bagus Arya <aryabagus453@gmail.com>
2024-11-04 12:07:52 +0800

    CRUD

--- app.js
+++ app.js
@@ -3,143 +3,142 @@ const app = express();
 const port = 3000;
 const path = require("path");
 require("./libs/hbs-helper");
-const config = require("./config/config.json")
-const { QueryTypes, Sequelize} = require("sequelize");
+const config = require("./config/config.json");
+const { QueryTypes, Sequelize } = require("sequelize");
 const sequelize = new Sequelize(config.development);
 
-app.set("view engine", "hbs")
+app.set("view engine", "hbs");
 
-app.use("/assets", express.static(path.join(__dirname, "./assets")))
-app.use("/assets/js", express.static("assets/js"))
-app.use("/assets/png", express.static("assets/png"))
-app.use("/assets/svg", express.static("assets/svg"))
-app.use("/views", express.static("views"))
-
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
 app.use(express.urlencoded({ extended: true }));
-
+// Rute yang digunakan
 app.get("/", home);
 app.get("/testimonial", testimonial);
 app.get("/contact", contact);
 app.get("/project", project);
 app.post("/project", projectPost);
-app.post("/delete-project/:index", projectDelete);
-app.get("/project-detail/:index", projectDetail);
-app.get("/update-project/:index", updateProject);
-app.post("/update-project/:index", updateProjectPost);
+app.post("/delete-project/:id", projectDelete);
+app.get("/project-detail/:id", projectDetail);
+app.get("/update-project/:id", updateProject);
+app.post("/update-project/:id", updateProjectPost);
 
+
+// Mengambil data proyek untuk halaman home
+async function home(req, res) {
+    const query = `SELECT * FROM tb_projects`;
+    let projects = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+    projects = projects.map((project) => ({
+        ...project,
+        technologies: project.technologies,
+        timePost: project.timePost,
+    }));
+
+    res.render("index", { projects });
+}
 
 
 const projects = [];
 
-function home (req, res){
-    res.render("index", {projects})
-};
-
-async function project(req, res) {
-  const query = `SELECT * FROM tb_projects`;
-  let projects = await sequelize.query(query, { type: QueryTypes.SELECT});
-
-  projects = projects.map((project)=> ({
-    ...project,
-  }));
-
-  res.render("index", {projects});
+function testimonial(req, res) {
+    res.render("testimonial");
 }
 
-function project (req, res){
-    res.render("add-project")
-};
+function contact(req, res) {
+    res.render("contact");
+}
 
-function testimonial (req, res){
-    res.render("testimonial")
-};
-function contact (req, res){
-    res.render("contact")
-};
+function project(req, res) {
+    res.render("add-project");
+}
 
-async function projectDetail(req, res) {
-    const { id } = req.params;
-
-    const query = `SELECT * FROM projects WHERE id = ${id}`
-    const project = await sequelize.query(query, { type: QueryTypes.SELECT });
-
-    project[0].author = "Bagus Arya";
-    res.render("project-detail", { project: project[0] });
-  }
-  
-async function projectPost(req, res) {
-    const { title, desc } = req.body;
-
-    const query = `INSERT INTO tb_projects(name,description,image) VALUES('${title}','${desc}', 'https://cdn.idntimes.com/content-images/duniaku/post/20240102/53e15-16596004347246-31d0481229478e18754dbe4327003280.jpeg')`
-
-    await sequelize.query(query, { type:QueryTypes.INSERT })
-
-    res.redirect("/");
-    // projects.unshift({
-    //     title,
-    //     desc,
-    //     createdAt: new Date(),
-    //     image:
-    //       "/assets/png/game-web.png",
-    //   });
+async function projectPost(req, res) { 
+    const { title, desc, technologies, start_date, end_date } = req.body;
     
-      
+    const techArray = Array.isArray(technologies)
+    ? technologies
+    : typeof technologies === "string"
+    ? technologies.split(',').map(tech => tech.trim())
+    : [];
 
-    console.log("Title : ", title);
-    console.log("Description : ", desc);
-  
-    // res.json(req.body);
+    const formattedTechnologies = `{${techArray.join(',')}}`;
+
+    const query = `
+        INSERT INTO tb_projects (name, description, image, technologies, start_date, end_date) 
+        VALUES ('${title}', '${desc}', 'https://i.namu.wiki/i/QPnRfQqdbrtD7DyUd5FFhdQs43lrN55S58tiNP9ghjuj5KwEEKAamZk20ch3uyWMK3wQg8M4KtvyfDMh80kdHg.webp', '${formattedTechnologies}', '${start_date}', '${end_date}')
+    `;
+        await sequelize.query(query, {
+            type: QueryTypes.INSERT
+        });
+        
+        res.redirect("/");
 }
 
 
 async function projectDelete(req, res) {
     const { id } = req.params;
   
-    const query = `DELETE FROM projects WHERE id=${id}`
-    await sequelize.query(query, { type: QueryTypes.DELETE})  
-    // projects.splice(index, 1);
+    const query = `DELETE FROM tb_projects WHERE id=${id}`;
+    await sequelize.query(query, { type: QueryTypes.DELETE });
   
     res.redirect("/");
-  }
-  
+}
+
+async function projectDetail(req, res) {
+    const { id } = req.params;
+
+    const query = `SELECT * FROM tb_projects WHERE id = :id`;
+    const project = await sequelize.query(query, { 
+        type: QueryTypes.SELECT, 
+        replacements: { id } 
+    });
+
+    if (project.length > 0) {
+        project[0].author = "Bagus Arya";
+        res.render("project-detail", { project: project[0] });
+    } else {
+        res.redirect("/");
+    }
+}
+
 async function updateProject(req, res) {
     const { id } = req.params;
-  
-    const query = `SELECT * FROM projects WHERE id=$(id)`
-    const project = await sequelize.query(query, { type: QueryTypes.SELECT});
-    project[0].author = "Bagus Arya"
-    
-    // const project = projects.find((_, idx) => idx == index);
-  
-    res.render("update-project", { project: project[0] });
-  }
 
+    const query = `SELECT * FROM tb_projects WHERE id=${id}`;
+    const project = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+    if (project.length > 0) {
+        project[0].author = "Bagus Arya";
+        res.render("update-project", { project: project[0] });
+    } else {
+        res.redirect("/");
+    }
+}
 async function updateProjectPost(req, res) {
-    const { index } = req.params;
-  
-    const { title, desc } = req.body;
+    const { id } = req.params;
+    const { title, desc, start_date, end_date, technologies } = req.body;
 
-    const query = `UPDATE projects SET title='${title}', description='${desc} WHERE id${id}`;
-    await sequelize.query(query, {type: QueryTypes.UPDATE})
-  
-    // projects[index] = {
-    //   title,
-    //     desc,
-    //     createdAt: new Date(),
-    //     image:
-    //       "/assets/png/game-web.png",
-    //   };
-  
-    res.redirect("/")
-  }
-// app.get("/", (req, res) => {
-//     const nilai = req.query.nilai
-//     res.send(nilai);
-// });
+    const query = `
+        UPDATE tb_projects 
+        SET name = $title, 
+            description = $desc, 
+            start_date = $start_date, 
+            end_date = $end_date, 
+            technologies = $technologies
+        WHERE id = $id
+    `;
+        await sequelize.query(query, {
+            type: QueryTypes.UPDATE,
+            bind: { title, desc, start_date, end_date, technologies, id }
+        });
 
-app.get("/about", (req, res) => {
-    res.send("Ini adalah halaman about");
-});
+        res.redirect("/");
+}
+
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- libs/hbs-helper.js
+++ libs/hbs-helper.js
@@ -1,55 +1,121 @@
+// helpers.js
 const hbs = require("hbs");
+// const moment = require("moment");
 
-hbs.registerHelper("get_full_time", (date) => {
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+// hbs.registerHelper("get_full_time", (date) => {
+//   if (!date) {
+//     return "Invalid date";
+//   }
 
-  const tanggal = date.getDate();
-  const bulan = months[date.getMonth()];
-  const tahun = date.getFullYear();
+//   const dateObj = moment(date);
+//   if (!dateObj.isValid()) {
+//     return "Invalid date";
+//   }
 
-  let jam = date.getHours();
-  let menit = date.getMinutes();
+//   return dateObj.format("DD MMM YYYY HH:mm [WIB]");
+// });
 
-  if (jam < 10) {
-    jam = "0" + jam;
-  }
+hbs.registerHelper("get_project_duration", (start, end) => {
+  if (!start || !end) return "Tanggal tidak valid";
 
-  if (menit < 10) {
-    menit = "0" + menit;
-  }
+  const startDate = new Date(start);
+  const endDate = new Date(end);
 
-  return `${tanggal} ${bulan} ${tahun} ${jam}:${menit} WIB`;
+  if (isNaN(startDate) || isNaN(endDate)) return "Tanggal tidak valid";
+
+  let totalDays = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
+  const months = Math.floor(totalDays / 30);
+  totalDays %= 30;
+  const days = totalDays;
+
+  let result = "";
+  if (months > 0) result += `${months} bulan `;
+  if (days > 0) result += `${days} hari`;
+
+  return result.trim() || "0 hari";
 });
 
+
+// hbs.registerHelper("get_full_time", (date) => {
+//   if (!date) {
+//     return "Invalid date";
+//   }
+
+//   const dateObj = new Date(date);
+//   if (isNaN(dateObj.getTime())) {
+//     return "Invalid date";
+//   }
+
+//   const months = [
+//     "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+//     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+//   ];
+
+//   const tanggal = dateObj.getDate();
+//   const bulan = months[dateObj.getMonth()];
+//   const tahun = dateObj.getFullYear();
+
+//   let jam = dateObj.getHours();
+//   let menit = dateObj.getMinutes();
+
+//   if (jam < 10) {
+//     jam = "0" + jam;
+//   }
+
+//   if (menit < 10) {
+//     menit = "0" + menit;
+//   }
+
+//   return `${tanggal} ${bulan} ${tahun} ${jam}:${menit} WIB`;
+// });
+
 hbs.registerHelper("get_distance_time", (timePost) => {
+  console.log("Type of timePost:", typeof timePost);
+  console.log("Value of timePost:", timePost);
+
+  if (!timePost || (typeof timePost !== "string" && !(timePost instanceof Date))) {
+      return "Tanggal tidak valid";
+  }
+
+  // Jika timePost adalah string, coba ubah menjadi objek Date
+  if (typeof timePost === "string") {
+      timePost = new Date(timePost);
+  }
+
+  // Cek apakah timePost adalah objek Date yang valid
+  if (isNaN(timePost.getTime())) {
+      return "Tanggal tidak valid";
+  }
+
   const timeNow = new Date();
-  const distance = timeNow - timePost; 
+  const distance = timeNow - timePost;
 
   const seconds = Math.floor(distance / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
-  const day = Math.floor(hours / 24);
+  const days = Math.floor(hours / 24);
 
   if (seconds < 60) {
-    return `${seconds} seconds ago`;
+      return `${seconds} detik yang lalu`;
   } else if (minutes < 60) {
-    return `${minutes} minutes ago`;
-  } else if (hours < 60) {
-    return `${hours} hours ago`;
-  } else if (day < 24) {
-    return `${day} day ago`;
+      return `${minutes} menit yang lalu`;
+  } else if (hours < 24) {
+      return `${hours} jam yang lalu`;
+  } else {
+      return `${days} hari yang lalu`;
   }
 });
+
+
+
+hbs.registerHelper('techIcon', function (techName) {
+  const icons = {
+      html: '<img class="img-icon" src="../assets/svg/html5.svg" >',
+      css: '<img class="img-icon" src="../assets/svg/css3.svg" width="17">',
+      react: '<img class="img-icon" src="../assets/svg/reactjs.svg">',
+      javascript: '<img class="img-icon" src="../assets/svg/javascript.svg">',
+  };
+  return new hbs.SafeString(icons[techName] || "");
+});
+
+module.exports = hbs;
