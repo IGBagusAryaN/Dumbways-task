c1ffc818415e6b1d69811cbff983500d61799d16
Bagus Arya <aryabagus453@gmail.com>
2024-11-10 15:38:44 +0800

    fix: deployment

--- config/config.js
+++ config/config.js
@@ -6,7 +6,8 @@ module.exports = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST,  // pastikan ini berasal dari variabel environment
+    port: process.env.POSTGRES_PORT || 5432,
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
@@ -21,6 +22,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT || 5432,
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
