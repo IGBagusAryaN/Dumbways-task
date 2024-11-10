542476d2844378add3db4eea0a549bacb4ba88cf
Bagus Arya <aryabagus453@gmail.com>
2024-11-10 13:54:47 +0800

    fix: deployment

--- config/config.js
+++ config/config.js
@@ -1,21 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
+const pg = require("pg");
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'postgres',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'postgres',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
     dialectOptions: {
       ssl: {
         require: true,
